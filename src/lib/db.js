4bda807dbbbcec3b231f71717f89d6f85fb1815c
Iver E. Johnson <Iver@grottenveien.no>
2026-02-26 17:08:45 +0100

    ...

--- src/lib/db.js
+++ src/lib/db.js
@@ -8,7 +8,6 @@ const pool = mariadb.createPool({
      host: process.env.DB_HOST, 
      user: process.env.DB_USER, 
      password: process.env.DB_PASSWORD,
-     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '5'),
      database: process.env.DB_NAME
 });
 
